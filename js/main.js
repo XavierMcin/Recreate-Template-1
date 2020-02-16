eec093396404ac5275aba4c854e71741603c265e
Javier <xavier.mcin@gmail.com>
2020-02-16 00:49:31 -0500

    finished tablet styles

--- js/main.js
+++ js/main.js
@@ -10,6 +10,17 @@ let dot2 = document.querySelector('#nav-dots li:nth-of-type(2)');
 let dot3 = document.querySelector('#nav-dots li:nth-of-type(3)');
 let dot4 = document.querySelector('#nav-dots li:last-of-type');
 
+let sliderPhoto = document.querySelector('.home section #image-slider .slide-pics div .img-container');
+let sliderNumber = document.querySelector('.home section #image-slider .slide-title div:first-of-type ul li');
+let sliderHeader = document.querySelector('.home section #image-slider .slide-title div:nth-of-type(2) .h1-wrapper h1');
+let sliderButton = document.querySelector('.home section #image-slider .slide-title div:last-of-type ul li');
+
+
+let photoWidth = (sliderPhoto.offsetWidth) + 70;
+let numberWidth = sliderNumber.offsetWidth;
+let headerHeight = sliderHeader.offsetHeight;
+let buttonHeight = sliderButton.offsetHeight;
+
 let headerDiv = document.querySelector('main section #image-slider .slide-title div:nth-of-type(2) .h1-wrapper');
 let numbersDiv = document.querySelector('main section #image-slider .slide-title div:first-of-type ul');
 let viewDiv = document.querySelector('main section #image-slider .slide-title div:last-of-type ul');
@@ -20,7 +31,7 @@ let Nstyle = getComputedStyle(numbersDiv);
 let Vstyle = getComputedStyle(viewDiv);
 let Pstyle = getComputedStyle(photosDiv);
 
-
+console.log(buttonHeight);
 
 dot1.onclick = function() {
     let Hcurr = parseInt(Hstyle.top);
@@ -31,20 +42,20 @@ dot1.onclick = function() {
     if (Hcurr == -510) {
         console.log('nothing');
     } else if (Hcurr == -340) {
-        headerDiv.style.top = (Hcurr - 170) + "px";
-        numbersDiv.style.left = (Ncurr + 240) + "px";
-        viewDiv.style.top = (Vcurr + 72) + "px";
-        photosDiv.style.left = (Pcurr + 630) + "px";
+        headerDiv.style.top = (Hcurr - headerHeight) + "px";
+        numbersDiv.style.left = (Ncurr + numberWidth) + "px";
+        viewDiv.style.top = (Vcurr + buttonHeight) + "px";
+        photosDiv.style.left = (Pcurr + photoWidth) + "px";
     } else if (Hcurr == -170) {
-        headerDiv.style.top = (Hcurr - 340) + "px";
-        numbersDiv.style.left = (Ncurr + 480) + "px";
-        viewDiv.style.top = (Vcurr + 144) + "px";
-        photosDiv.style.left = (Pcurr + 1260) + "px";
+        headerDiv.style.top = (Hcurr - (headerHeight * 2)) + "px";
+        numbersDiv.style.left = (Ncurr + (numberWidth * 2)) + "px";
+        viewDiv.style.top = (Vcurr + (buttonHeight * 2)) + "px";
+        photosDiv.style.left = (Pcurr + (photoWidth * 2)) + "px";
     } else if (Hcurr == 0) {
-        headerDiv.style.top = (Hcurr - 510) + "px";
-        numbersDiv.style.left = (Ncurr + 720) + "px";
-        viewDiv.style.top = (Vcurr + 216) + "px";
-        photosDiv.style.left = (Pcurr + 1890) + "px";
+        headerDiv.style.top = (Hcurr - (headerHeight * 3)) + "px";
+        numbersDiv.style.left = (Ncurr + (numberWidth * 3)) + "px";
+        viewDiv.style.top = (Vcurr + (buttonHeight * 3)) + "px";
+        photosDiv.style.left = (Pcurr + (photoWidth * 3)) + "px";
     }
 
 }
@@ -56,22 +67,22 @@ dot2.onclick = function() {
     let Pcurr = parseInt(Pstyle.left);
     
     if (Hcurr == -510) {
-        headerDiv.style.top = (Hcurr + 170) + "px";
-        numbersDiv.style.left = (Ncurr - 240) + "px";
-        viewDiv.style.top = (Vcurr - 72) + "px";
-        photosDiv.style.left = (Pcurr - 630) + "px";
+        headerDiv.style.top = (Hcurr + headerHeight) + "px";
+        numbersDiv.style.left = (Ncurr - numberWidth) + "px";
+        viewDiv.style.top = (Vcurr - buttonHeight) + "px";
+        photosDiv.style.left = (Pcurr - photoWidth) + "px";
     } else if (Hcurr == -340) {
         console.log('nothing');
     } else if (Hcurr == -170) {
-        headerDiv.style.top = (Hcurr - 170) + "px";
-        numbersDiv.style.left = (Ncurr + 240) + "px";
-        viewDiv.style.top = (Vcurr + 72) + "px";
-        photosDiv.style.left = (Pcurr + 630) + "px";
+        headerDiv.style.top = (Hcurr - headerHeight) + "px";
+        numbersDiv.style.left = (Ncurr + numberWidth) + "px";
+        viewDiv.style.top = (Vcurr + buttonHeight) + "px";
+        photosDiv.style.left = (Pcurr + photoWidth) + "px";
     } else if (Hcurr == 0) {
-        headerDiv.style.top = (Hcurr - 340) + "px";
-        numbersDiv.style.left = (Ncurr + 480) + "px";
-        viewDiv.style.top = (Vcurr + 144) + "px";
-        photosDiv.style.left = (Pcurr + 1260) + "px";
+        headerDiv.style.top = (Hcurr - (headerHeight * 2)) + "px";
+        numbersDiv.style.left = (Ncurr + (numberWidth * 2)) + "px";
+        viewDiv.style.top = (Vcurr + (buttonHeight * 2)) + "px";
+        photosDiv.style.left = (Pcurr + (photoWidth * 2)) + "px";
     }
 
 }
@@ -83,22 +94,22 @@ dot3.onclick = function() {
     let Pcurr = parseInt(Pstyle.left);
     
     if (Hcurr == -510) {
-        headerDiv.style.top = (Hcurr + 340) + "px";
-        numbersDiv.style.left = (Ncurr - 480) + "px";
-        viewDiv.style.top = (Vcurr - 144) + "px";
-        photosDiv.style.left = (Pcurr - 1260) + "px";
+        headerDiv.style.top = (Hcurr + (headerHeight * 2)) + "px";
+        numbersDiv.style.left = (Ncurr - (numberWidth * 2)) + "px";
+        viewDiv.style.top = (Vcurr - (buttonHeight * 2)) + "px";
+        photosDiv.style.left = (Pcurr - (photoWidth * 2)) + "px";
     } else if (Hcurr == -340) {
-        headerDiv.style.top = (Hcurr + 170) + "px";
-        numbersDiv.style.left = (Ncurr - 240) + "px";
-        viewDiv.style.top = (Vcurr - 72) + "px";
-        photosDiv.style.left = (Pcurr - 630) + "px";
+        headerDiv.style.top = (Hcurr + headerHeight) + "px";
+        numbersDiv.style.left = (Ncurr - numberWidth) + "px";
+        viewDiv.style.top = (Vcurr - buttonHeight) + "px";
+        photosDiv.style.left = (Pcurr - photoWidth) + "px";
     } else if (Hcurr == -170) {
         console.log('nothing');
     } else if (Hcurr == 0) {
-        headerDiv.style.top = (Hcurr - 170) + "px";
-        numbersDiv.style.left = (Ncurr + 240) + "px";
-        viewDiv.style.top = (Vcurr + 72) + "px";
-        photosDiv.style.left = (Pcurr + 630) + "px";
+        headerDiv.style.top = (Hcurr - headerHeight) + "px";
+        numbersDiv.style.left = (Ncurr + numberWidth) + "px";
+        viewDiv.style.top = (Vcurr + buttonHeight) + "px";
+        photosDiv.style.left = (Pcurr + photoWidth) + "px";
     }
 
 }
@@ -110,20 +121,20 @@ dot4.onclick = function() {
     let Pcurr = parseInt(Pstyle.left);
     
     if (Hcurr == -510) {
-        headerDiv.style.top = (Hcurr + 510) + "px";
-        numbersDiv.style.left = (Ncurr - 720) + "px";
-        viewDiv.style.top = (Vcurr - 216) + "px";
-        photosDiv.style.left = (Pcurr - 1890) + "px";
+        headerDiv.style.top = (Hcurr + (headerHeight * 3)) + "px";
+        numbersDiv.style.left = (Ncurr - (numberWidth * 3)) + "px";
+        viewDiv.style.top = (Vcurr - (buttonHeight * 3)) + "px";
+        photosDiv.style.left = (Pcurr - (photoWidth * 3)) + "px";
     } else if (Hcurr == -340) {
-        headerDiv.style.top = (Hcurr + 340) + "px";
-        numbersDiv.style.left = (Ncurr - 480) + "px";
-        viewDiv.style.top = (Vcurr - 144) + "px";
-        photosDiv.style.left = (Pcurr - 1260) + "px";
+        headerDiv.style.top = (Hcurr + (headerHeight * 2)) + "px";
+        numbersDiv.style.left = (Ncurr - (numberWidth * 2)) + "px";
+        viewDiv.style.top = (Vcurr - (buttonHeight * 2)) + "px";
+        photosDiv.style.left = (Pcurr - (photoWidth * 2)) + "px";
     } else if (Hcurr == -170) {
-        headerDiv.style.top = (Hcurr + 170) + "px";
-        numbersDiv.style.left = (Ncurr - 240) + "px";
-        viewDiv.style.top = (Vcurr - 72) + "px";
-        photosDiv.style.left = (Pcurr - 630) + "px";
+        headerDiv.style.top = (Hcurr + headerHeight) + "px";
+        numbersDiv.style.left = (Ncurr - numberWidth) + "px";
+        viewDiv.style.top = (Vcurr - buttonHeight) + "px";
+        photosDiv.style.left = (Pcurr - photoWidth) + "px";
     } else if (Hcurr == 0) {
         console.log('nothing');
     }
